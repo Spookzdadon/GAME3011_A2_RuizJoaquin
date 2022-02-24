f209de90eea2bf789033d7c2fd4ef66618f8ff30
Joaquin <56128205+Spookzdadon@users.noreply.github.com>
2022-02-24 17:27:56 -0500

    Added timer, lock pick amount, functional difficulty and player skill, and canvas with all UI elements

--- Assets/Scripts/LockPickingScript.cs
+++ Assets/Scripts/LockPickingScript.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public enum Difficulty
+{
+    EASY,
+    MEDIUM,
+    HARD
+}
 
 public class LockPickingScript : MonoBehaviour
 {
+    [Header("Lock Attributes")]
     [SerializeField]
     private float pickSpeed;
     [SerializeField]
@@ -11,9 +21,20 @@ public class LockPickingScript : MonoBehaviour
     [SerializeField]
     private float lockRetentionSpeed;
     [SerializeField]
-    private float lockleniency;
+    private float lockLeniency;
     [SerializeField]
     private float tensionMultiplier;
+    [SerializeField]
+    private float playerSkill = 0;
+    [SerializeField]
+    private Difficulty difficulty = Difficulty.EASY;
+
+    [Header("Object References")]
+    public TextMeshProUGUI lockDifficultyText;
+    public TextMeshProUGUI playerSkillText;
+    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI lockPickText;
+    public TextMeshProUGUI textBox;
 
     private Animator animator;
     private float lockPosition;
@@ -22,20 +43,23 @@ public class LockPickingScript : MonoBehaviour
     private float targetPosition;
     private bool shaking;
     private float tension = 0f;
+    private float timeValue = 120;
+    private int lockPickAmount = 3;
     
 
+    
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();   
+    }
     private void Start()
     {
         LockPosition = 0;
         PickPosition = 0;
         tension = 0f;
-
         targetPosition = Random.value;
-    }
-
-    private void Awake()
-    {
-        animator = GetComponent<Animator>();   
+        lockPickText.text = lockPickAmount.ToString();
     }
 
     private void Update()
@@ -49,6 +73,7 @@ public class LockPickingScript : MonoBehaviour
             Shaking();
             Lock();
             UpdateAnimator();
+            Timer();
         }
     }
 
@@ -58,7 +83,7 @@ public class LockPickingScript : MonoBehaviour
         if (shaking)
         {
             tension += Time.deltaTime * tensionMultiplier;
-            if (tension > 1f)
+            if (tension > 1.5f)
             {
                 BreakPick();
             }
@@ -68,15 +93,28 @@ public class LockPickingScript : MonoBehaviour
     private void BreakPick()
     {
         Debug.Log("Lock pick broken");
+        //textBox.text = "Lock pick broke!";
+        SetTextBox("Lock pick broke!");
         shaking = false;
-        gameDone = true;
+        LockPosition = 0;
+        PickPosition = 0;
+        lockPickAmount -= 1;
+        if (lockPickAmount <=0)
+        {
+            lockPickAmount = 0;
+            textBox.text = "You ran out of lock picks!";
+            gameDone = true;
+        }
+        tension = 0f;
+        lockPickText.text = lockPickAmount.ToString();
+
     }
 
     private float MaxRotationDistance
     {
         get
         {
-            return 1f - Mathf.Abs(targetPosition - PickPosition) + lockleniency;
+            return 1f - Mathf.Abs(targetPosition - PickPosition) + lockLeniency + (playerSkill / 100);
         }
     }
 
@@ -120,15 +158,98 @@ public class LockPickingScript : MonoBehaviour
     private void Win()
     {
         gameDone = true;
+        textBox.text = "You opened the lock!";
+        //SetTextBox("You opened the lock!");
         print("You opened the lock!");
     }
 
     public void ResetGame()
     {
+        lockPickAmount = 3;
+        timeValue = 120f;
         LockPosition = 0;
         PickPosition = 0;
         tension = 0f;
+        textBox.text = "";
+        lockPickText.text = lockPickAmount.ToString();
         gameDone = false;
+    }
+
+    public void SetDifficulty()
+    {
+        switch (difficulty)
+        {
+            case Difficulty.EASY:
+                lockLeniency = 0.0005f;
+                lockDifficultyText.text = "Medium";
+                difficulty = Difficulty.MEDIUM;
+                break;
+            case Difficulty.MEDIUM:
+                lockLeniency = 0.0001f;
+                lockDifficultyText.text = "Hard";
+                difficulty = Difficulty.HARD;
+                break;
+            case Difficulty.HARD:
+                lockLeniency = 0.001f;
+                lockDifficultyText.text = "Easy";
+                difficulty = Difficulty.EASY;
+                break;
+        }
+
+    }
+
+    public void IncreasePlayerSkill()
+    {
+        if (playerSkill >= 99)
+        {
+            return;
+        }
+        playerSkill += 1f;
+        int floatToInt = (int)(playerSkill);
+        playerSkillText.text = (floatToInt).ToString();
+    }
+
+    public void DecreasePlayerSkill()
+    {
+        if (playerSkill <= 0)
+        {
+            return;
+        }
+        playerSkill -= 1f;
+        int floatToInt = (int)(playerSkill);
+        playerSkillText.text = (floatToInt).ToString();
+    }
+
+    private void Timer()
+    {
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            timeValue = 0;
+            gameDone = true;
+            textBox.text = "You ran out of time!";
+        }
+
+        float minutes = Mathf.FloorToInt(timeValue / 60);
+        float seconds = Mathf.FloorToInt(timeValue % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    private void SetTextBox(string msg)
+    {
+        StopAllCoroutines();
+        StartCoroutine(SetText(msg));
+    }
+
+    IEnumerator SetText(string msg)
+    {
+        textBox.text = msg;
+        yield return new WaitForSeconds(2);
+        textBox.text = "";
     }
 
     private void UpdateAnimator()
